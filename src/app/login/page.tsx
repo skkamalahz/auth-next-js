60ebc291516e5f081b647c44c4c6898b6f41bcea
SK Kamal AHZ Web Dev <kamal@ukcoursehunter.com>
2025-03-09 14:05:44 +0600

    Signup and Login Complete - 02:08:54

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,21 +1,47 @@
 "use client";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { Axios } from "axios";
+import axios, { Axios } from "axios";
+import toast from "react-hot-toast";
 
 
 export default function LoginPage() {
+    const router = useRouter();
     const [user, setUser] = React.useState({
         email: "",
         password: "",
     });
 
-    const onLogin = async () => { };
+    const [buttonDisabled, setButtonDisabled] = React.useState(false);
+    const [Loading, setLoading] = React.useState(false);
+
+    const onLogin = async () => { 
+        try {
+            setLoading(true);
+            const response = await axios.post("/api/users/login", user);
+            console.log("Login Success", response.data);
+            toast.success("Login successful");
+            router.push("../profile");
+        } catch (error: any) {
+            console.log("Login Error", error.message);
+            toast.error(error.message);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        if (user.email.length > 0 && user.password.length > 0) {
+            setButtonDisabled(false);
+        } else {
+            setButtonDisabled(true);
+        }
+    }, [user]);
 
     return (
         <div className="flex flex-col items-center min-h-screen justify-center py-2">
-            <h1 className="text-center text-2xl">Login Page</h1>
+            <h1 className="text-center text-2xl">{Loading ? "Logging in..." : "Login Page"}</h1>
             <hr />
             <hr />
 
