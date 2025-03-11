8cd1663b9915ea4f118f9c3e0d1f5470226e2c3b
SK Kamal AHZ Web Dev <kamal@ukcoursehunter.com>
2025-03-11 15:46:14 +0600

    continue after 03.40.00

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -3,7 +3,7 @@ export default function UserProfile({params}: any) {
         <div className="flex flex-col items-center min-h-screen justify-center py-2">
             <h1 className="text-center text-2xl">Profile</h1>
             <hr />
-            <p className="text-4xl mt-4 mb-4">Profile Page <span className=" p-2 rounded bg-orange-500 text-black">{params.id}</span></p>
+            <p className="text-4xl mt-4 mb-4">Profile Page <span className=" p-2 rounded bg-green-700 text-black">{params.id}</span></p>
             <hr />
         </div>
     );
